104fb078d475376bcb38a1c01e625a7d15563539
Glass <glass@ideum.com>
2016-06-28 05:46:00 -0600

    adj meta tags

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 <head>
 
 <meta charset="UTF-8"/>
-<meta name="robots" content="noindex, nofollow">
+<meta name="robots" content="index, follow">
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
